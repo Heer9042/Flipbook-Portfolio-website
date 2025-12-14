7ef9b378cdce06ca68d947007d3d3d565cc0b53d
heer patel <heerpatel904242@gmail.com>
2025-12-14 13:24:30 +0530

    update Certificate

--- javaScript/main.js
+++ javaScript/main.js
@@ -20,38 +20,9 @@ pageTurnBtn.forEach((el, index) => {
 });
 
 const pages = document.querySelectorAll(".book-page.page-right");
-const contactMeBtn = document.querySelector(".btn.contact-me");
-
-contactMeBtn.onclick = () => {
-    pages.forEach((page, index) => {
-        setTimeout(() => {
-            page.classList.add("turn");
-
-            setTimeout(() => {
-                page.style.zIndex = 20 + index;
-            }, 500);
-        }, (index + 1) * 200 + 100);
-    });
-};
 
 let totalPages = pages.length;
 let pageNumber = 0;
-
-const backProfileButton = document.querySelector(".back-profile");
-
-backProfileButton.onclick = () => {
-    pages.forEach((_, index) => {
-        setTimeout(() => {
-            reverseIndex();
-            pages[pageNumber].classList.remove("turn");
-
-            setTimeout(() => {
-                reverseIndex();
-                pages[pageNumber].style.zIndex = 10 + index;
-            }, 500);
-        }, (index + 1) * 200 + 100);
-    });
-};
 
 function reverseIndex() {
     pageNumber--;
